9fd2260463222f69af276e0eb32f290547a3e9b0
umerkang66 <umerkang66@gmail.com>
2022-05-11 11:45:38 +0500

    update fib.js

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -4,23 +4,6 @@ import axios from 'axios';
 class Fib extends Component {
     state = { seenIndexes: [], values: {}, index: '' };
 
-    componentDidMount() {
-        this.fetchValues();
-        this.fetchIndexes();
-    }
-
-    async fetchValues() {
-        const values = await axios.get('/api/values/current');
-
-        this.setState({ values: values.data });
-    }
-
-    async fetchIndexes() {
-        const seenIndexes = await axios.get('/api/values/all');
-
-        this.setState({ seenIndexes: seenIndexes.data });
-    }
-
     renderSeenIndexes() {
         return this.state.seenIndexes.map(({ number }) => number).join(', ');
     }
@@ -40,9 +23,9 @@ class Fib extends Component {
 
     async handleSubmit(e) {
         e.preventDefault();
-        await axios.post('/api/values', {
-            index: this.state.index,
-        });
+        // await axios.post('/api/values', {
+        //     index: this.state.index,
+        // });
         this.setState({ index: '' });
     }
 
